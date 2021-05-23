bccd3e2ee9971ae85eb6a977876b0a5d59ab7a46
Beam1626 <53890290+Beam1626@users.noreply.github.com>
2021-05-23 23:47:20 +0700

    Update regisemp.php

--- regis/regisemp.php
+++ regis/regisemp.php
@@ -1,6 +1,11 @@
-<?php include("connec.php");?>
-<?php
+<?php include("connec.php");
 
+if (isset($_POST['line_id'])) {
+    
+    $line_id = $_POST['line_id'];
+} else {
+    $line_id = null;
+}
 if (isset($_POST['first_name'])) {
     $first_name = $_POST['first_name'];
     //echo $first_name;
@@ -12,6 +17,11 @@ if (isset($_POST['last_name'])) {
      //echo $last_name;
 } else {
     $last_name = null;
+}
+if (isset($_POST['birthday'])) {
+    $birthday = $_POST['birthday'];
+} else {
+    $birthday = null;
 }
 
 if (isset($_POST['gender'])) {
@@ -25,23 +35,22 @@ if (isset($_POST['phone'])) {
 } else {
     $phone = null;
 }
+$timestamp =date("Y-m-d H:i:s");
 
     
-     //$id_line = $_POST["id_line"];
-     $timestamp =date("Y-m-d H:i:s");
-     $first_name = $_POST["first_name"];
-     $last_name = $_POST["last_name"]; 
-     $birthday = date("Y-m-d ");  	 
-     $gender = $_POST["gender"];    
-     $phone = $_POST["phone"];	 
-
+    //  $line_id = $_POST["line_id"];
+    //  $first_name = $_POST["first_name"];
+    //  $last_name = $_POST["last_name"]; 
+    //  $birthday = date("Y-m-d ");  	 
+    //  $gender = $_POST["gender"];    
+    //  $phone = $_POST["phone"];	 
      
-     $str = "INSERT INTO customer (timeD,name_cus,surname_cus,birthday,gender,tel_cus) 
-     VALUES ('$timestamp','$first_name','$last_name','$birthday','$gender','$phone')";
+     $str = "INSERT INTO customer (line_id,name_cus,surname_cus,birthday,gender,tel_cus,timeD) 
+     VALUES ('$line_id','$first_name','$last_name','$birthday','$gender','$phone','$timestamp')";
     
      if ($conn->query($str) === TRUE) {
-        echo "New record created successfully";
+        echo 0;
       } else {
-        echo "Error: " . $sql . "<br>" . $conn->error;
+        echo 1;
       }
 ?>
